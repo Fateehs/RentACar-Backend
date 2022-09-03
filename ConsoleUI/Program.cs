e5717ae2f4c258c8b3475adbab7344a373dbdff5
Fateehs <benzer1010@gmail.com>
2022-09-03 17:31:05 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Business.Concrete;
+using DataAccess.Concrete;
+
+namespace ConsoleUI
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Description);
+            }
+        }
+    }
+}
---
+++ Entities/Concrete/Car.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities.Abstract;
+
+namespace Entities.Concrete
+{
+    public class Car : IEntity
+    {
+        public int CarId { get; set; }
+
+        public int BrandId { get; set; }
+        public int ColorId { get; set; }
+
+        public int ModelYear { get; set; }
+
+        public int DailyPrice { get; set; }
+
+        public string Description { get; set; }
+        
+    }
+}
