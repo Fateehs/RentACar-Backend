df40b9dcda7a436282a5dfc3d754227540904d2c
Fatih Enes Selvi <benzer1010@gmail.com>
2022-11-18 22:58:39 +0300

    Custom Exception for File added

---
+++ Core/Exceptions/FileHelperCustomException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Exceptions
+{
+    public class FileHelperCustomException : Exception, ICustomException
+    {
+        public FileHelperCustomException(string message) : base(message)
+        {
+
+        }
+    }
+}
